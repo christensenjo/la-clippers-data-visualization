f7a91497ff923bd16236e53d05c1a2f4c54961ef
Joel Christensen <christensenjo14@gmail.com>
2024-09-12 17:53:55 -0600

    Initial database setup with dotenv

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ source .venv/bin/activate # On Windows use .venv\Scripts\activate
 pip install -r requirements.txt
 ```
 
+## Setting up environment variables locally
+
+To run locally, create a `.env` file patterned after the `.env.example` file.
+```
+DB_NAME=<lac_fullstack_dev>
+DB_USER=<your_username>
+DB_PASSWORD=<your_password>
+DB_HOST=localhost
+DB_PORT=5432
+```
--- la_clippers_project/settings.py
+++ la_clippers_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +28,8 @@ SECRET_KEY = 'django-insecure-q)(t66+%ugl%ctx4ekb2z-bmdg_%ywhqph=pq2l!65xk&tshmn
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+load_dotenv()  # Load environment variables from .env file
 
 
 # Application definition
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'la_clippers_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
